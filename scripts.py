b39ba237920fa304cdb4e044a7add85a21320074
Farid <famimahe2003@gmail.com>
2024-10-31 09:03:18 -0600

    reconfigurando el formato para el agarre de datos correctos para la api titles

--- scripts.py
+++ scripts.py
@@ -4,18 +4,30 @@ from bs4 import BeautifulSoup
 import re
 app = Flask(__name__)
 
-# Ruta principal que renderiza el HTML
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/news')
+def api_news():
+    url = "https://www.lavanguardia.com/deportes/resultados"
+    response = requests.get(url)
+    news = []
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        news = [a_tag.get_text() for h2_tag in soup.find_all('h2','title') for a_tag in h2_tag.find_all('a','page-link')]
+
+    
+    return jsonify(news)
 
 
 
 
 def parse_match_info(text):
     # Patrón para partidos programados (e.g., "nombre_equipo 10h00 nombre_equipo2")
-    pattern_time = r"(\w+(?:\s\w+)*)\s(\d{1,2}h\d{2})\s(\w+(?:\s\w+)*)"
-    # Patrón para resultados finales (e.g., "nombre_equipo 0-1 nombre_equipo2")
+    pattern_time = r"([A-Za-z]+)\s*(\d{1,2}h\d{2})\s*([A-Za-z]+)"
+     # Patrón para resultados finales (e.g., "nombre_equipo 0-1 nombre_equipo2")
     pattern_score = r"(\w+(?:\s\w+)*)\s(\d+)-(\d+)\s(\w+(?:\s\w+)*)"
     
     match_time = re.match(pattern_time, text)
@@ -27,6 +39,8 @@ def parse_match_info(text):
             "hora": hora,
             "equipo2": equipo2
         }
+    
+    return {"error": "Formato no reconocido"}
     
     match_score = re.match(pattern_score, text)
     if match_score:
@@ -43,7 +57,7 @@ def parse_match_info(text):
 
 # Nueva ruta para obtener solo los datos en formato JSON
 @app.route('/api/titles')
-def api_news():
+def api_titles():
     url = "https://www.lavanguardia.com/deportes/resultados"
     response = requests.get(url)
     results = []
@@ -56,23 +70,12 @@ def api_news():
         # Procesar cada elemento encontrado
         for element in match_elements:
             match_text = element.get_text(strip=True)  # Obtener el texto completo del elemento
+            print(match_text)
             match_info = parse_match_info(match_text)  # Analizar el texto
             results.append(match_info)  # Agregar el resultado a la lista
 
     return jsonify(results)
 
-
-@app.route('/api/news')
-def api_news():
-    url = "https://www.lavanguardia.com/deportes/resultados"
-    response = requests.get(url)
-    news = []
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        news = [a_tag.get_text() for h2_tag in soup.find_all('h2','title') for a_tag in h2_tag.find_all('a','page-link')]
-
-    
-    return jsonify(news)
 
 
 
